bd645e96c9dc5e3e50f52b367e9e477490f72105
mikaelgramont <mgramont@gmail.com>
2015-12-08 15:03:28 -0800

    Made it possible to pass optional configuration when instantiating show-and-tell object.

--- scripts/webvr-showandtell.js
+++ scripts/webvr-showandtell.js
@@ -1,13 +1,39 @@
-WebVRShowAndTell = function() {
+WebVRShowAndTell = function(userConfig, logger) {
+	this.defaultConfig_ = {
+		modelPath: null,
+		modelTexture: null,
+		skyBoxTexture: 'images/box.png',
+		skyBoxWidth: 5,
+		skyBoxRepeat: 5,
+	};
+
+	this.config = {};
+	var prop;
+	for (prop in this.defaultConfig_) {
+		if (this.defaultConfig_.hasOwnProperty(prop)) {
+			this.config[prop] = this.defaultConfig_[prop];
+		}
+	}
+
+	for (prop in userConfig) {
+		if (userConfig.hasOwnProperty(prop) && this.defaultConfig_.hasOwnProperty(prop)) {
+			this.config[prop] = userConfig[prop];
+		}
+	}
+
+	var nullLogger = {log: function(){}};
+	this.logger_ = logger || nullLogger;
 };
 
-WebVRShowAndTell.prototype.setup = function() {
-	this.setupGeneral_();
+WebVRShowAndTell.prototype.init = function() {
+	this.setupLoading_();
+
 	this.setupRenderer_();
 	this.setupScene_();
-	this.setupCamera_();
-	this.setupVRManager_();
 	this.setupLights_();
+	this.setupCamera_();
+	
+	this.setupVRManager_();
 	this.setupMainObject_();
 	this.setupSkybox_();
 	
@@ -16,22 +42,33 @@ WebVRShowAndTell.prototype.setup = function() {
 	this.animate_();
 };
 
-WebVRShowAndTell.prototype.setupGeneral_ = function() {
+WebVRShowAndTell.prototype.setupLoading_ = function() {
 	this.loadingManager = new THREE.LoadingManager();
-	this.loadingManager.onProgress = function(item, loaded, total) {
-	  console.log(item, loaded, total);
-	};
+	this.loadingManager.onProgress = (function(item, loaded, total) {
+	  this.logger_.log(item, loaded, total);
+	}).bind(this);
+
+	this.imageLoader = new THREE.ImageLoader(this.loadingManager);
+	this.objLoader = new THREE.OBJLoader(this.loadingManager);
 };
 
 WebVRShowAndTell.prototype.setupRenderer_ = function() {
 	this.renderer = new THREE.WebGLRenderer({antialias: true});
 	this.renderer.setPixelRatio(window.devicePixelRatio);
 	document.body.appendChild(this.renderer.domElement);
-	
 };
 
 WebVRShowAndTell.prototype.setupScene_ = function() {
 	this.scene = new THREE.Scene();
+};
+
+WebVRShowAndTell.prototype.setupLights_ = function() {
+	var ambient = new THREE.AmbientLight(0x101030);
+	this.scene.add(ambient);
+
+	var directionalLight = new THREE.DirectionalLight(0xffeedd);
+	directionalLight.position.set(0, 0, 1);
+	this.scene.add(directionalLight);
 };
 
 WebVRShowAndTell.prototype.setupCamera_ = function() {
@@ -51,33 +88,22 @@ WebVRShowAndTell.prototype.setupVRManager_ = function() {
 	this.VRManager = new WebVRManager(this.renderer, this.effect, {hideButton: false});
 };
 
-WebVRShowAndTell.prototype.setupLights_ = function() {
-	var ambient = new THREE.AmbientLight(0x101030);
-	this.scene.add(ambient);
-
-	var directionalLight = new THREE.DirectionalLight(0xffeedd);
-	directionalLight.position.set(0, 0, 1);
-	this.scene.add(directionalLight);
-};
-
 WebVRShowAndTell.prototype.setupMainObject_ = function() {
-	var onProgress = function(xhr) {
+	var onProgress = (function(xhr) {
 	  if (xhr.lengthComputable) {
 	    var percentComplete = xhr.loaded / xhr.total * 100;
-	    console.log(Math.round(percentComplete, 2) + '% downloaded');
+	    this.logger_.log(Math.round(percentComplete, 2) + '% downloaded');
 	  }
-	};
+	}).bind(this);
 	var onError = function(xhr) {};	
 
-	var imageLoader = new THREE.ImageLoader(this.loadingManager);
 	this.objTexture = new THREE.Texture();
-	imageLoader.load('models/UV_Grid_Sm.jpg', (function(image) {
+	this.imageLoader.load(this.config.modelTexture, (function(image) {
 	  this.objTexture.image = image;
 	  this.objTexture.needsUpdate = true;
 	}).bind(this));
 
-	var objLoader = new THREE.OBJLoader(this.loadingManager);
-	objLoader.load('models/male02.obj', this.onObjectLoaded_.bind(this), onProgress, onError);	
+	this.objLoader.load(this.config.modelPath, this.onObjectLoaded_.bind(this), onProgress, onError);	
 };
 
 WebVRShowAndTell.prototype.onObjectLoaded_ = function(object) {
@@ -90,27 +116,39 @@ WebVRShowAndTell.prototype.onObjectLoaded_ = function(object) {
     this.mainObject.position.set(0, -.5, -1);
     this.mainObject.scale.set(.005, .005, .005);
     this.scene.add(this.mainObject);
-
 };
 
 WebVRShowAndTell.prototype.setupSkybox_ = function() {
-	var boxWidth = 5;
-	var texture = THREE.ImageUtils.loadTexture('images/box.png');
-	texture.wrapS = THREE.RepeatWrapping;
-	texture.wrapT = THREE.RepeatWrapping;
-	texture.repeat.set(boxWidth, boxWidth);
+	if (this.config.skyBoxTexture) {		
+		this.skyBoxTexture = new THREE.Texture();
+		this.imageLoader.load(this.config.skyBoxTexture, (function(image) {
+		  this.skyBoxTexture.image = image;
+		  this.skyBoxTexture.needsUpdate = true;			
+		}).bind(this));
 
-	var geometry = new THREE.BoxGeometry(boxWidth, boxWidth, boxWidth);
-	var material = new THREE.MeshBasicMaterial({
-	  map: texture,
-	  color: 0x01BE00,
-	  side: THREE.BackSide
-	});
+		if (this.config.skyBoxRepeat) {
+			this.skyBoxTexture.wrapS = THREE.RepeatWrapping;
+			this.skyBoxTexture.wrapT = THREE.RepeatWrapping;
+			this.skyBoxTexture.repeat.set(this.config.skyBoxRepeat, this.config.skyBoxRepeat);
+		}
 
-	var skybox = new THREE.Mesh(geometry, material);
-	this.scene.add(skybox);
+		var boxWidth = this.config.skyBoxWidth;
+		var geometry = new THREE.BoxGeometry(boxWidth, boxWidth, boxWidth);
+
+		var material = new THREE.MeshBasicMaterial({
+		  map: this.skyBoxTexture,
+		  color: 0x01BE00,
+		  side: THREE.BackSide
+		});
+
+		var skybox = new THREE.Mesh(geometry, material);
+		this.scene.add(skybox);
+	}
 };
 
+/**
+ * This is WIP, but the goal is to enable gaze-based input for Cardboard.
+ */
 WebVRShowAndTell.prototype.setupGazeInput_ = function(timestamp) {
 	this.renderer.domElement.addEventListener('click', (function(event) {
 	  var vector = new THREE.Vector3();
@@ -120,7 +158,7 @@ WebVRShowAndTell.prototype.setupGazeInput_ = function(timestamp) {
 	  vector.unproject(this.camera);
 	  raycaster.set(this.camera.position, vector.sub(this.camera.position).normalize());
 	  var intersects = raycaster.intersectObject(this.mainObject, true);
-	  console.log(intersects.length > 0 ? "click intersect" : "click no intersect")
+	  this.logger_.log(intersects.length > 0 ? "click intersect" : "click no intersect")
 	}).bind(this));
 };
 
@@ -129,6 +167,5 @@ WebVRShowAndTell.prototype.animate_ = function(timestamp) {
 
   this.VRManager.render(this.scene, this.camera, timestamp);
 
-  requestAnimationFrame(this.animate_.bind(this));
-	
+  requestAnimationFrame(this.animate_.bind(this));	
 };
